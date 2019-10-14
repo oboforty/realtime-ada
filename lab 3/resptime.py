906d970e1d5ec48eec09de295dcf1910bbd32604
oboforty <rajmund.csombordi@gmail.com>
2019-10-14 11:17:16 +0200

    Final diagrams & fpscalc

--- lab 3/resptime.py
+++ lab 3/resptime.py
@@ -9,29 +9,33 @@ dict values in order:
   [J] jitter time: value for Jitter, [Vmin, Vmax] for Jitter range, leave empty for no Jitter
   [B] block time: leave Jitter empty (None or '-') if you have this value, but not Jitter!
 """
-tasks = {
-  "τA": [5, 20, 10, 5],
-  "τB": [30, 50, 50, 10],
-}
-
 # tasks = {
-#   "τ1": [2, 20, 6],
-#   "τ2": [3, 7, 7],
-#   "τ3": [5, 14, 13],
-#   "τ4": [4, 100, 60],
+#   "τA": [5, 20, 10, 5],
+#   "τB": [30, 50, 50, 10],
 # }
+
+tasks = {
+  "τ1": [2, 20, 6],
+  "τ2": [3, 7, 7],
+  "τ3": [5, 14, 13],
+  "τ4": [4, 100, 60],
+}
 
 prioRM = fpscalc.get_prio(tasks, key=fpscalc.RM)
 prioDM = fpscalc.get_prio(tasks, key=fpscalc.DM)
+
+prioCustom = ["τ2", "τ1", "τ3", "τ4"]
 
 print("RM prio:", prioRM)
 print("DM prio:", prioDM)
 
 for i in tasks:
-    Ri1 = fpscalc.get_resp(i, tasks, prio=prioRM)
-    Ri2 = fpscalc.get_resp(i, tasks, prio=prioDM)
+    #Ri1 = fpscalc.get_resp(i, tasks, prio=prioRM)
+    #Ri2 = fpscalc.get_resp(i, tasks, prio=prioDM)
 
-    print("{}:    Ri RM: {}     Ri DM {}".format(i, Ri1, Ri2))
+    #print("{}:    Ri RM: {}     Ri DM {}".format(i, Ri1, Ri2))
+
+    print("{}: Ri = {}".format(i, fpscalc.get_resp(i, tasks, prio=prioCustom)))
 
 
 print("\nTotal utilization of tasks: {}\nSystem bound: {}".format(prio_ord(tasks), util_bound(tasks)))
